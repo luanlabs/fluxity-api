6ca350890ea73622eb34c9ce54954d8e61c819db
Ferit Kahraman <feritkahraman32135@gmail.com>
2023-10-22 22:55:44 +0330

    feat : Building the overall structure

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express from 'express';
 import db from './database/db';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+import indexHandler from './routes/token/indexHandler';
 
 const app = express();
 const port = 3000;
+
+//connect database
 db();
 
-app.get('/', (req, res) => {
-  res.send('ggg');
-});
+//env file
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', indexHandler);
+
+// app.get('/', bodyParser.urlencoded(), index); If there is a body request
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-console.log('a');
