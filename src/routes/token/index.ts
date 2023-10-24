b31b22f77552dbfcbce42d899fdc3086d8e19442
Ferit Kahraman <feritkahraman32135@gmail.com>
2023-10-24 19:40:47 +0330

    fix: add mint route

--- src/routes/token/index.ts
+++ src/routes/token/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import addTokenRoute from './addToken';
+import mintToken from './mintToken';
 
 const router = express.Router();
 
 router.get('/add', addTokenRoute);
+router.post('/mint', mintToken);
 
 export default router;
