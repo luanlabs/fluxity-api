b1d7ec4c576cf5d893e0f28e7dbf630c4e0eeb27
Omid Kayhani <o.kayhani@gmail.com>
2023-10-27 22:38:39 +0300

    update: add subscription routes to the router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import tokenRoutes from './token';
+import subscribeRoutes from './subscribe';
 
 const router = express.Router();
 
 router.use('/token', tokenRoutes);
+router.use('/subscribe', subscribeRoutes);
 
 export default router;
