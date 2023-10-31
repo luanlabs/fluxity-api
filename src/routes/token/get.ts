b5045d08b2a422a46e301ebf3adf926c51fb4a90
Ferit Kahraman <feritkahraman32135@gmail.com>
2023-10-31 23:08:24 +0330

    feat: add try and catch to get tokens, add swagger for get tokens

--- src/routes/token/get.ts
+++ src/routes/token/get.ts
@@ -3,11 +3,19 @@ import { RequestHandler } from 'express';
 import Token from '../../models/Token';
 
 const getToken: RequestHandler = async (req, res) => {
-  const tokens = await Token.find({});
-  return res.status(200).j({
-    status: 'success',
-    message: 'Tokens have been successfully found',
-    result: tokens,
-  });
+  try {
+    const tokens = await Token.find({});
+    return res.status(200).j({
+      status: 'success',
+      message: 'Tokens have been successfully found',
+      result: tokens,
+    });
+  } catch (e) {
+    return res.status(500).j({
+      status: 'error',
+      message: e.message,
+      result: {},
+    });
+  }
 };
 export default getToken;
