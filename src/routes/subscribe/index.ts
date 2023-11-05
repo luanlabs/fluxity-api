f73241edc9cb7050fc809ecf3b96ce648e85212f
Omid Kayhani <o.kayhani@gmail.com>
2023-11-05 18:43:12 +0000

    fix: add admin authentication to get subscribers

--- src/routes/subscribe/index.ts
+++ src/routes/subscribe/index.ts
@@ -2,10 +2,11 @@ import express from 'express';
 
 import addSubscriber from './add';
 import getSubscribers from './get';
+import authAdmin from '../../middleware/authAdmin';
 
 const router = express.Router();
 
 router.post('/', addSubscriber);
-router.get('/', getSubscribers);
+router.get('/', authAdmin, getSubscribers);
 
 export default router;
