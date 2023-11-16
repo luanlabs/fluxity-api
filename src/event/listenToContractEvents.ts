5b347caf13acc1d373c2b80542798d8b7cf87b60
Ferit Kahraman <feritkahraman32135@gmail.com>
2023-11-16 12:16:59 +0330

    fix: insert if for events

--- src/event/listenToContractEvents.ts
+++ src/event/listenToContractEvents.ts
@@ -39,21 +39,23 @@ const listenToContractEvents = async () => {
         },
       });
 
-      const eventsXdr = events.result.events;
+      if (events) {
+        const eventsXdr = events.result.events;
 
-      for (let i = 0; i < events.result.events.length; i++) {
-        const streamId = ToScVal.fromXDR(eventsXdr[i].value.xdr);
+        for (let i = 0; i < events.result.events.length; i++) {
+          const streamId = ToScVal.fromXDR(eventsXdr[i].value.xdr);
 
-        if (eventsXdr[i].topic[1] === created) {
-          await saveNewStream(streamId);
-        } else if (eventsXdr[i].topic[1] === withdrawn) {
-          await saveStreamWithdrawn(streamId);
-        } else if (eventsXdr[i].topic[1] === cancelled) {
-          await cancelStream(streamId);
+          if (eventsXdr[i].topic[1] === created) {
+            await saveNewStream(streamId);
+          } else if (eventsXdr[i].topic[1] === withdrawn) {
+            await saveStreamWithdrawn(streamId);
+          } else if (eventsXdr[i].topic[1] === cancelled) {
+            await cancelStream(streamId);
+          }
         }
-      }
 
-      lastUsedLedger = events.result.latestLedger;
+        lastUsedLedger = events.result.latestLedger;
+      }
     }, 15000);
   } catch (e) {}
 };
