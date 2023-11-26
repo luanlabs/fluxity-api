cb7ad1854a30206878dfa874154ab66201fb4b7e
Ferit Kahraman <feritkahraman32135@gmail.com>
2023-11-26 19:58:19 +0330

    chore: change code

--- src/utils/soroban/stream/calculateLastUsedLedger.ts
+++ src/utils/soroban/stream/calculateLastUsedLedger.ts
@@ -1,5 +1,4 @@
 import Ledger from '../../../models/Ledger';
-import getServer from '../getServer';
 
 const calculateLastUsedLedger = async (sequence: number) => {
   const lastSavedLedger = await Ledger.findOne({ _id: '0' });
@@ -8,13 +7,10 @@ const calculateLastUsedLedger = async (sequence: number) => {
     return sequence - 6;
   }
 
-  const server = getServer();
-  const oldLedger = await server.getTransaction(
-    'c4515e3bdc0897f21cc5dbec8c82cf0a936d4741cb74a8e158eb51b9fb00411a',
-  );
+  const oldLedger = sequence - 1440;
 
-  if (Number(lastSavedLedger.last) <= Number(oldLedger.oldestLedger)) {
-    return sequence - 6;
+  if (Number(lastSavedLedger.last) <= oldLedger) {
+    return sequence - 1438;
   }
 
   return Number(lastSavedLedger.last);
