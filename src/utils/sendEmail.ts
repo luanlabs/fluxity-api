2fd351892c97dd9ff8870431d778e3f4d9c65343
Ferit Kahraman <feritkahraman32135@gmail.com>
2023-11-30 16:27:44 +0330

    fix: move transporter variable to function

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,17 +1,17 @@
 import nodemailer from 'nodemailer';
 
-// Create a transporter for sending emails
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.MAILER_SERVICE_USER,
-    pass: process.env.MAC_MAILER_SERVICE_PASS,
-  },
-});
-
 // Function to send an email
 const sendEmail = async (to: string, subject: string, text: string) => {
   try {
+    // Create a transporter for sending emails
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.MAILER_SERVICE_USER,
+        pass: process.env.MAC_MAILER_SERVICE_PASS,
+      },
+    });
+
     // Define the email options
     const mailOptions = {
       from: process.env.MAILER_SERVICE_USER,
