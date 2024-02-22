e15d46e7eaaf0be82481dba70bb788a0f919c8b2
Ferit Kahraman <feritkahraman32135@gmail.com>
2024-02-22 14:24:23 +0330

    chore: remove migrations

--- src/migrations/migrate.ts
+++ src/migrations/migrate.ts
@@ -1,11 +1,8 @@
 import log from '../logger';
-import tokenUp from './add_network_field_to_token';
-import streamUp from './add_network_field_to_stream';
 
 const migrate = async () => {
   try {
-    await tokenUp();
-    await streamUp();
+    return;
   } catch (error) {
     log.error({ message: 'Migration failed:', error });
   }
