be9d12f3cddd0a390f7b69e29d84e75aa76017c9
Ferit Kahraman <feritkahraman32135@gmail.com>
2024-04-04 00:50:30 +0330

    feat: add completed and cancelled

--- src/routes/stream/getStreams.ts
+++ src/routes/stream/getStreams.ts
@@ -17,7 +17,11 @@ const getStreamsRoute: RequestHandler = async (req, res) => {
     let streams = streamAll.map((stream) => stream.toObject());
 
     for (let i = 0; i < streams.length; i++) {
-      streams[i].status = calculateStreamStatus(streams[i].start_date, streams[i].end_date);
+      streams[i].status = calculateStreamStatus(
+        streams[i].start_date,
+        streams[i].end_date,
+        streams[i].cancelled_date,
+      );
     }
 
     if (status) {
